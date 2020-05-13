3bb5c817dfd4471d32543c76babbb2594828c21b
acnubhav <51119316+acnubhav@users.noreply.github.com>
2020-05-13 12:57:36 +0530

    build

---
+++ build/precache-manifest.3fc95761430d50fe3d11c7126f6921dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5c453bb7a3773fdffac3766a2a826d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "372594d88e7354027d99",
+    "url": "/static/css/main.9dad37d3.chunk.css"
+  },
+  {
+    "revision": "cb95857ecba416e78b6d",
+    "url": "/static/js/2.0ccf0285.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.0ccf0285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372594d88e7354027d99",
+    "url": "/static/js/main.a9b2a6ed.chunk.js"
+  },
+  {
+    "revision": "8d0a98eb7fd2f4ecd6b6",
+    "url": "/static/js/runtime-main.d4bdd09a.js"
+  },
+  {
+    "revision": "2c4e8ea0d838a55e8bbd48ead3ef37df",
+    "url": "/static/media/Anubhav.2c4e8ea0.png"
+  }
+]);
